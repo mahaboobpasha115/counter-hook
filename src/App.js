3ea71e2d9ded4f7bec143f02154c8ac0244943c4
mahaboobpasha115 <mahaboobpashapasha115@gmail.com>
2021-02-04 20:51:06 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, {useState} from "react";
 
 function App() {
+ const initialCount = 0;
+ const[count ,setCount]= useState(initialCount);
 
-
-  const [count, incrementCount]= useState(0); 
+ const incrementFive=()=>{
+   for(let i=0;i<5;i++){
+     setCount(prevCount=>prevCount+1)
+   }
+ }
 
   return (
     <div className="App">
-     <button onClick={()=>incrementCount(count+1)}>count {count}</button>
+     count: {count}
+     <button onClick={()=>setCount(prevCount => prevCount + 1)}>increment</button>
+     <button onClick={()=>setCount(prevCount => prevCount - 1)}>decrement</button>
+     <button onClick={incrementFive}>increment 5</button>
+     <button onClick={()=>setCount(initialCount)}>reset</button>
     </div>
   );
 }
